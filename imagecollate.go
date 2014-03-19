82419a6c01659e68f588c545b09dae4e8ff7fccd
Andrew Reece <azmreece@gmail.com>
2014-03-19 00:28:32 +0000

    converts downloaded images to pdf in conventional format (A4 etc) or scaled to image

--- imagecollate.go
+++ imagecollate.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//~ "code.google.com/p/gofpdf"
+	"code.google.com/p/gofpdf"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -17,35 +17,75 @@ func main() {
 	folder := "/home/andrew/pics/imgcollate" // Files are saved in a subfolder of this called title's string
 	title := "MitchellSystemSleeves"
 	firstFile := 1 // After slice of files collected, all files from firstFile to the end of the slice are downloaded. '1', not '0' is the first possible file.
+	format := "A4" //"scale", "A3", "A4", "A5", "Letter", or "Legal"
 	
-	collectImages(url, folder, title, firstFile)
-	//~ convertToPDF(url, title)
+	images := collectImages(url, folder, title, firstFile)
+	convertToPDF(url, folder, title, images, format)
 }
 
-func collectImages(url, containingFolder, title string, startNum int) {
-	sourceHTML := getHTML(url)
-	//~ fmt.Println(sourceHTML)
-	
-	jpg := regexp.MustCompile("src=[\"']([^\"']*\\.jpe?g)[\"']")
-	imgUrls := jpg.FindAllStringSubmatch(sourceHTML, -1)
-	imgUrlsLen := len(imgUrls)
-	dlLen := len(imgUrls[startNum-1:])
+// Downloads all the images (currently just jpgs) from the url to a subfolder of name title in containingFolder
+func collectImages(sourceUrl, containingFolder, title string, startNum int) []imgSource {
+	sourceHTML := getHTML(sourceUrl)
 	
 	folder := containingFolder + "/" + title
 	os.MkdirAll(folder, 0755)
 	configureLogger(folder + "/" + "dl.txt", "", 0)
+	
+	var imgSources []imgSource
+	
+	// Finds all jpe?g files in HTML
+	jpg := regexp.MustCompile("src=[\"']([^\"']*(\\.jpe?g))[\"']")
+	imgURLs := jpg.FindAllStringSubmatch(sourceHTML, -1)
+	imgURLsLen := len(imgURLs)
+	dlLen := len(imgURLs[startNum-1:])
+	
 	for i := startNum; i <= dlLen; i++{
-		zeroedI := leadZeros(i, imgUrlsLen)
-		filename := title + zeroedI
-		dlImg(imgUrls[i-1][1], folder + "/" + filename)
-		fmt.Printf("%v of %d downloading; %v of %d total\n", leadZeros(i+1-startNum, dlLen), dlLen, zeroedI, imgUrlsLen)
-		log.Printf("%v\t-\t%v", filename, imgUrls[i-1][1])
+		imgURL := imgURLs[i-1][1]
+		zeroedI := leadZeros(i, imgURLsLen)
+		extension := imgURLs[i-1][2]
+		filename := title + zeroedI + extension
+		fileStr := folder + "/" + filename
+		
+		dlImg(imgURL, fileStr)
+		fmt.Printf("%v of %d downloading; %v of %d total\n", leadZeros(i+1-startNum, dlLen), dlLen, zeroedI, imgURLsLen)
+		log.Printf("%v\t-\t%v", fileStr, imgURL)
+		imgSources = append(imgSources, imgSource{FileStr: fileStr, URL: imgURL})
+	}
+	return imgSources
+}
+
+// Converts selection of images in a folder to a pdf and outputs that in the same folder
+func convertToPDF(pageUrl, containingFolder, filename string, imgSources []imgSource, format string) {
+	outputFolder := containingFolder + "/" + filename // In case I later want to change output
+	
+	pdf := gofpdf.New("P", "mm", format, "/usr/share/fonts/")
+	if format == "scale" {
+		pdf = gofpdf.New("P", "mm", "A4", "/usr/share/fonts/")
+	}
+	
+	for i, img := range imgSources {
+		fmt.Println(img.FileStr)
+		makeImagePage(pdf, format, img, i)
+		// makes the page the same dimensions as the image
+		
+	}
+	err := pdf.OutputFileAndClose(outputFolder + "/" + filename + ".pdf")
+	if err != nil {
+		fmt.Println(err)
 	}
 }
 
-func dlImg(url, file string) {
+type imgSource struct {
+	FileStr string
+	URL     string
+}
+
+
+
+// Wraps GETting and 'saving' an image as a download
+func dlImg(url, fileStr string) {
 	img := getIMG(url)
-	ioutil.WriteFile(file, img, 0755)
+	ioutil.WriteFile(fileStr, img, 0755)
 }
 
 // Returns a string of the HTML from the URL provided
@@ -61,6 +101,7 @@ func getHTML(url string) string{
 	}
 	return string(HTML)
 }
+// GETs an image from its url and returns as a []byte
 func getIMG(url string) []byte{
 	resp, err := http.Get(url)
 	if err != nil {
@@ -74,13 +115,37 @@ func getIMG(url string) []byte{
 	return img
 }
 
+
+func makeImagePage(pdf *gofpdf.Fpdf, format string, img imgSource, i int) {
+	imgInfo := pdf.RegisterImage(img.FileStr, "")
+	imgSize := gofpdf.SizeType{Wd: imgInfo.Width(), Ht: imgInfo.Height()}
+	if format == "scale" {
+			pdf.AddPageFormat("", imgSize)
+			//func (f *Fpdf) Image(fileStr string, x, y, w, h float64, flow bool, tp string, link int, linkStr string)
+			pdf.Image(img.FileStr, 0, 0, imgSize.Wd, imgSize.Ht, false, "", 0, img.URL)
+		} else {
+			pdf.AddPage()
+			pgW, pgH, _ := pdf.PageSize(i)
+			if imgSize.Wd/imgSize.Ht > pgW/pgH {
+				if imgSize.Wd > pgW {
+					imgSize.Wd = pgW
+				}
+				pdf.Image(img.FileStr, 0, 0, imgSize.Wd, 0, false, "", 0, img.URL)
+			} else {
+				if imgSize.Ht > pgH {
+					imgSize.Ht = pgH
+				}
+				pdf.Image(img.FileStr, 0, 0, 0, imgSize.Ht, false, "", 0, img.URL)
+			}
+		}
+}
+
 func printSliceSlice(sl [][]string) {
     fmt.Printf("Slice length = %d\r\n", len(sl))
     for i := 0; i < len(sl); i++ {
         fmt.Println(sl[i][1])
     }
 }
-
 
 // Returns string with prepended leading zeroes so that currentNum has the same numDigits as the highest number in the list
 func leadZeros(currentNum, maxNum int) string {
@@ -93,20 +158,19 @@ func leadZeros(currentNum, maxNum int) string {
 	}
 	return intAsStr
 }
+// Convenience function that returns the number of difits in a %d representation of a number. e.g 84 in gives 2 out
 func numDigits(i int) int {
 	return len(strconv.Itoa(i))
 }
 
-func configureLogger(filename string, prefix string, flags int) {
-	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
+func configureLogger(fileStr string, prefix string, flags int) {
+	file, err := os.OpenFile(fileStr, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
 		if err != nil {
-			panic (err)
+			fmt.Print (err)
 		}
-	//defer file.Close() Doesn't work with this uncommented... why?
 	log.SetOutput(file)
 	log.SetPrefix(prefix)
 	log.SetFlags(flags)
 }
-//~ func convertToPDF(url, name string) {
-	//~ 
-//~ }
+
+
